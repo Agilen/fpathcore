58405986cc2d0fc743f07cc62b8d22a5f4030a3c
fontan2312 <fontan2312@gmail.com>
2021-06-05 14:59:32 +0300

    initial commit

---
+++ Ffpath/Algor.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Ffpath
+{
+    class Algor
+    {
+        public void Ping(int n, int st, int[,] w, int end, List<string> IPA)
+        {
+            string writePath = "\\Users\\fonta\\source\\repos\\Ffpath\\Ffpath\\Data\\res.txt";
+            string[] Dd = new string[n];
+            //string buf = "";
+            bool[,] visited = new bool[n, n];
+            int[,] D = new int[n, n];
+
+            for (int k = 0; k < IPA.Count(); k++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Dd[i] = "";
+                    D[k, i] = w[k, i];
+                    visited[k, i] = false;
+                }
+
+                D[k, k] = 0;
+                int index = 0, u = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    int min = int.MaxValue;
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (!visited[k, j] && D[k, j] < min)
+                        {
+                            min = D[k, j];
+                            index = j;
+                        }
+                    }
+
+                    u = index;
+                    visited[k, u] = true;
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (!visited[k, j] && w[u, j] != int.MaxValue && D[k, u] != int.MaxValue && (D[k, u] + w[u, j] < D[k, j]))
+                        {
+                            D[k, j] = D[k, u] + w[u, j];
+                            //buf = u.ToString() + " -> ";
+                            //Dd[j] += Dd[u]+u.ToString() + " -> ";
+                            //buf = " ";
+                        }
+                    }
+                }
+            }
+            string text = "";
+            if (D[st, end] != int.MaxValue)
+            {
+                text = $"Обмен пакетами с {IPA[end]} по 32 байт: \n" +
+                    $" Ответ от {IPA[end]}: число байт=32 время={(D[st, end] + D[end, st])} TTL=32" +
+                  $"\n Ответ от {IPA[end]}: число байт=32 время={(D[st, end] + D[end, st])} TTL=32" +
+                  $"\n Ответ от {IPA[end]}: число байт=32 время={(D[st, end] + D[end, st])} TTL=32" +
+                  $"\n Ответ от {IPA[end]}: число байт=32 время={(D[st, end] + D[end, st])} TTL=32";
+            }
+            else if (D[st, end] == int.MaxValue)
+                text = $"Обмен пакетами с {IPA[end]} по 32 байт:\n" +
+                    $"\n Ответ от {IPA[end]}: маршрут недоступен" +
+                    $"\n Ответ от {IPA[end]}: маршрут недоступен" +
+                    $"\n Ответ от {IPA[end]}: маршрут недоступен" +
+                    $"\n Ответ от {IPA[end]}: маршрут недоступен";
+
+            using (StreamWriter sw = new StreamWriter(writePath, false, Encoding.Default))
+            {
+                sw.WriteLine(text);
+            }
+
+        }
+
+        public void Dijkstra(int n, int st, int[,] w, List<string> IPA)
+        {
+            string[] Dd = new string[n];
+            string buf = "";
+            bool[,] visited = new bool[n, n];
+            int[,] D = new int[n, n];
+            for (int k = 0; k < IPA.Count(); k++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Dd[i] = "";
+                    D[k, i] = w[k, i];
+                    visited[k, i] = false;
+                }
+
+                D[k, k] = 0;
+                int index = 0, u = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    int min = int.MaxValue;
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (!visited[k, j] && D[k, j] < min)
+                        {
+                            min = D[k, j];
+                            index = j;
+                        }
+                    }
+
+                    u = index;
+                    visited[k, u] = true;
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (!visited[k, j] && w[u, j] != int.MaxValue && D[k, u] != int.MaxValue && (D[k, u] + w[u, j] < D[k, j]))
+                        {
+                            D[k, j] = D[k, u] + w[u, j];
+                            //buf = u.ToString() + " -> ";
+                            //Dd[j] += Dd[u]+u.ToString() + " -> ";
+                            //buf = " ";
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Стоимость пути из начальной вершины до остальных(Алгоритм Дейкстры):\t\n");
+
+            int[] b = new int[n * n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (D[i, j] < int.MaxValue)
+                    {
+                        b[j * i + j] = D[i, j];
+                    }
+                }
+            }
+            int max = b.Max();
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (D[i, j] != int.MaxValue && D[i, j] == max)
+                        Console.WriteLine((IPA[i]) + " -> " /*+ Dd[i]*/ + (IPA[j]) + " = " + (D[i, j] + D[j, i]));
+                    //else if (D[i] == int.MaxValue)
+                    //    Console.WriteLine((IPA[st]) + " -> " + (IPA[i]) + " = " + "маршрут недоступен");
+                }
+            }
+
+        }
+    }
+    
+}
---
+++ Ffpath/Program.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Ffpath
+{
+    class Program
+    {
+        public class Foo
+        {
+            //[Index(0)]
+            public string A { get; set; }
+            //[Index(1)]
+            public string B { get; set; }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Algor al = new Algor();
+
+
+            var lines = File.ReadAllLines("\\Users\\fonta\\source\\repos\\Ffpath\\Ffpath\\Data\\G1.csv");
+            var list = new List<Foo>();
+
+            foreach (var line in lines) //считываем данные из G1.csv
+            {
+                var value = line.Split(',');
+                var foo = new Foo() { A = value[0], B = value[1] };
+                list.Add(foo);
+            }
+
+
+            List<string> IPA = new List<string>();
+            List<string> IPB = new List<string>();
+
+
+            IEnumerable Ip = list.Select(c => c.A).Distinct().OrderBy(c => c);// сортирую и удаляю дубликаты для создания графа
+            IEnumerable Ip2 = list.Select(c => c.B).Distinct().OrderBy(c => c);
+
+            foreach (var ip in Ip)// перевожу IEnumerable в  List, что бы было удобнее работать
+            {
+                IPA.Add(ip.ToString());
+            }
+            foreach (var ip in Ip)
+            {
+                IPB.Add(ip.ToString());
+            }
+
+            int[,] graf = new int[IPA.Count(), IPB.Count()];// создаю граф сети
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                x = 0;
+                y = 0;
+                if (list[i].A == list[i].B)
+                {
+                    continue;
+                }
+                while (IPA[x] != list[i].A)
+                {
+                    x++;
+                }
+                while (IPB[y] != list[i].B)
+                {
+                    y++;
+                }
+                graf[x, y] = 2;
+                graf[y, x] = 2;
+            }
+
+
+            List<string> noCon = new List<string>();// проверка на узлы без соединений
+
+            for (int i = 0; i < IPA.Count(); i++)
+            {
+                for (int j = 0; j < IPB.Count(); j++)
+                {
+                    if (graf[i, j] != 0)
+                    {
+                        break;
+                    }
+                    else if (j == IPB.Count() - 1 && graf[i, j] == 0)
+                    {
+                        noCon.Add(IPB[i]);
+                    }
+                }
+            }
+
+            for (int i = 0; i < IPA.Count; i++)// подготавливаю граф для Алгоритма Дейкстра
+            {
+                for (int j = 0; j < IPB.Count(); j++)
+                {
+                    if (graf[i, j] == 0)
+                    {
+                        graf[i, j] = int.MaxValue;
+                    }
+                }
+            }
+
+            Console.WriteLine("1.Хост *192.168.0.5* під керуванням ОС *Debian 10* виконує `ping 192.168.0.1 -c 1 > res.txt`. Що буде записано в файлі `res.txt` ? :)");
+            int st = 0;
+            int end = 0;
+
+            for (int i = 0; i < IPA.Count(); i++)// 
+            {
+                if (IPA[i] == "192.168.0.5")
+                    st = i;
+                else if (IPA[i] == "192.168.0.1")
+                    end = i;
+            }
+
+            al.Ping(IPA.Count(), st, graf, end, IPA);
+            //
+            Console.WriteLine("2.Який максимальний час утиліта `ping` може видати в підмережі і між якими вузлами?");
+
+            al.Dijkstra(IPA.Count(), 0, graf, IPA);
+            //
+            Console.WriteLine("\n3.Чи існуючть вузли, між якими взагалі відсутнє мережеве з'єднання? Якщо так, то які?");
+
+            for (int i = 0; i < noCon.Count(); i++)
+            {
+                Console.WriteLine(noCon[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
